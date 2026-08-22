cb2f775fcfc392f21f06f918261c001c6c097a7e
traviscomber <n3uralia@gmail.com>
2026-08-22 16:11:28 -0700

    production: expose mine sector intelligence page

---
+++ app/dashboard/produccion/inteligencia/page.tsx
@@ -0,0 +1,5 @@
+import { MineSectorIntelligence } from '@/components/production/mine-sector-intelligence';
+
+export default function ProduccionInteligenciaPage() {
+  return <MineSectorIntelligence />;
+}
