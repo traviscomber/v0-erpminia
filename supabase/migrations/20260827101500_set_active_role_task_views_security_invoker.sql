bf7a0db2a476fb2d4421496e2488c841a0c1f0e8
traviscomber <n3uralia@gmail.com>
2026-08-27 10:09:26 -0400

    security: use invoker semantics for active task views

---
+++ supabase/migrations/20260827101500_set_active_role_task_views_security_invoker.sql
@@ -0,0 +1,14 @@
+alter view public.role_tasks_actionable_v1 set (security_invoker = true);
+alter view public.role_task_escalations_v1 set (security_invoker = true);
+alter view public.role_task_worklist_v1 set (security_invoker = true);
+alter view public.role_task_personal_inbox_v1 set (security_invoker = true);
+alter view public.role_task_operational_lanes_v1 set (security_invoker = true);
+alter view public.role_task_primary_inbox_v1 set (security_invoker = true);
+alter view public.role_task_primary_inbox_summary_v1 set (security_invoker = true);
+alter view public.role_task_actions_available_v1 set (security_invoker = true);
+alter view public.role_task_frontend_v1 set (security_invoker = true);
+alter view public.role_task_frontend_summary_v1 set (security_invoker = true);
+alter view public.role_task_daily_brief_v1 set (security_invoker = true);
+alter view public.role_task_action_catalog_v1 set (security_invoker = true);
+alter view public.role_task_worklist_summary_v1 set (security_invoker = true);
+alter view public.role_tasks_by_cargo_summary_v1 set (security_invoker = true);
